c50ff3b259fc8df5c900809177d6c34cc9cde3d2
Rajagopal B <rajagopal.b@optisolbusiness.com>
2023-07-06 11:48:28 +0530

    master

--- src/main/java/com/utils/screenshot.java
+++ src/main/java/com/utils/screenshot.java
@@ -1,15 +1,15 @@
-package com.utils;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
-import com.drivermanager.BaseClass;
-
-public final class screenshot extends BaseClass {
-
-	public static String captureScreenshot(){
-		 
-		 String screenone = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
-         return screenone;
-	}
-}
+//package com.utils;
+//
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
+//
+//import com.drivermanager.BaseClass;
+//
+//public final class screenshot extends BaseClass {
+//
+//	public static String captureScreenshot(){
+//		 
+//		 String screenone = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BASE64);
+//         return screenone;
+//	}
+//}
